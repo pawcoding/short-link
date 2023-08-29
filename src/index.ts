dc63b2c068ea9717c42dac509910485e4f63d646
pawcode Development <development@pawcode.de>
2023-08-29 07:52:33 +0200

    feat(setup): setup typescript compiler

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-const express = require('express')
-const dotenv = require('dotenv')
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv'
 
 dotenv.config()
 
-const app = express()
+const app: Express = express()
 const port = process.env.PORT
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server')
 })
 
